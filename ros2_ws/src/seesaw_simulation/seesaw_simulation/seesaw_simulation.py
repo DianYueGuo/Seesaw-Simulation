99a0c199adf2d836928a70446d6b2274dcb0a54f
DianYueGuo <dianyueguo@gmail.com>
2025-11-04 21:35:53 +0800

    add publishers and subscribers

--- ros2_ws/src/seesaw_simulation/seesaw_simulation/seesaw_simulation.py
+++ ros2_ws/src/seesaw_simulation/seesaw_simulation/seesaw_simulation.py
@@ -10,8 +10,20 @@ class SeesawSimulation(Node):
 
     def __init__(self):
         super().__init__('seesaw_simulation')
-        self.__slider_position_publisher = self.create_publisher(Float64, 'slider_position_m', 10)
-        self.__seesaw_angle_publisher = self.create_publisher(Float64, 'seesaw_angle_rad', 10)
+        self.__slider_radial_position_publisher = self.create_publisher(Float64, 'slider_radial_position_m', 10)
+        self.__slider_radial_velocity_publisher = self.create_publisher(Float64, 'slider_radial_velocity_m_per_s', 10)
+        self.__slider_angular_position_publisher = self.create_publisher(Float64, 'slider_angular_position_rad', 10)
+        self.__slider_angular_velocity_publisher = self.create_publisher(Float64, 'slider_angular_velocity_rad_per_s', 10)
+
+        self.__publisher_loop_frequency_Hz = 60.0
+        self.__publisher_loop_timer = self.create_timer(1 / self.__publisher_frequency_Hz, self.__publisher_loop_cb)
+
+        self.__applied_torque_subscription = self.create_subscription(
+            Float64,
+            'applied_torque_N_m',
+            self.__applied_torque_subscription_cb,
+            10
+        )
 
         self.__simulation_time_delta_s = 0.01
         self.__simulation_loop_timer = self.create_timer(self.__simulation_time_delta_s, self.__simulation_loop_cb)
@@ -26,6 +38,26 @@ class SeesawSimulation(Node):
         self.__angular_velocity_rad_per_s = 0.0
 
         self.__applied_torque_N_m = 0.0
+
+    def __publisher_loop_cb(self):
+        slider_radial_position_msg = Float64()
+        slider_radial_position_msg.data = self.__radial_position_m
+        self.__slider_radial_position_publisher.publish(slider_radial_position_msg)
+
+        slider_radial_velocity_msg = Float64()
+        slider_radial_position_msg.data = self.__radial_velocity_m_per_s
+        self.__slider_radial_velocity_publisher.publish(slider_radial_velocity_msg)
+
+        slider_angular_position_msg = Float64()
+        slider_angular_position_msg.data = self.__angular_position_rad
+        self.__slider_angular_position_publisher.publish(slider_angular_position_msg)
+
+        slider_angular_velocity_msg = Float64()
+        slider_angular_velocity_msg.data = self.__angular_velocity_rad_per_s
+        self.__slider_angular_velocity_publisher.publish(slider_angular_velocity_msg)
+
+    def __applied_torque_subscription_cb(self, msg):
+        self.__applied_torque_N_m = msg.data
 
     def __simulation_loop_cb(self):
         radial_acceleration_m_per_s_squared = (
