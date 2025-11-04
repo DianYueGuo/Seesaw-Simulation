1dba3dc85ca424f0648f3f7a4b8a4f1a6de7a7ef
DianYueGuo <dianyueguo@gmail.com>
2025-11-04 23:11:05 +0800

    log msg data

--- ros2_ws/src/seesaw_simulation/seesaw_simulation/gui/main.js
+++ ros2_ws/src/seesaw_simulation/seesaw_simulation/gui/main.js
@@ -1,5 +1,9 @@
 const websocket = new WebSocket("ws://" + window.location.hostname + "/websocket", "protocolOne");
 
+var slider_radial_position_m = 0.5;
+var slider_angular_position_rad = 0.0;
+
+
 websocket.onmessage = (event) => {
   console.log(event.data);
 
@@ -7,9 +11,11 @@ websocket.onmessage = (event) => {
 
     if (msg_json_object.type == "topic") {
         if (msg_json_object.data.topic_name == "slider_radial_position_m") {
-            var slider_radial_position_msg = msg_json_object.data.msg;
+            slider_radial_position_m = msg_json_object.data.msg.data;
+            console.log("slider_radial_position_m", slider_radial_position_m);
         } else if (msg_json_object.data.topic_name == "slider_angular_position_rad") {
-            var slider_angular_position_msg = msg_json_object.data.msg;
+            slider_angular_position_rad = msg_json_object.data.msg.data;
+            console.log("slider_angular_position_rad", slider_angular_position_rad);
         }
     }
 };
